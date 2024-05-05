d1ad21bcd0f08befdb723042930c4971cca8df9c
ignorancer0301 <93507007+ignorancer0301@users.noreply.github.com>
2024-05-06 00:42:00 +0800

    Basic file of Snake(Ver1.5）
    Ver1.5 Added the listener section in src
    Modified by Niu Xinpeng and Xiao Mingyuan

---
+++ src/cn/itcast/dragon/listener/GameListener.java
@@ -0,0 +1,30 @@
+package cn.itcast.dragon.listener;
+
+/**
+ * 
+ * 游戏监听器
+ * 
+ */
+public interface GameListener {
+
+	/**
+	 * 游戏开始了
+	 */
+	void gameStart();
+
+	/**
+	 * 游戏结束了
+	 */
+	void gameOver();
+
+	/**
+	 * 游戏暂停了
+	 */
+
+	void gamePause();
+
+	/**
+	 * 游戏继续
+	 */
+	void gameContinue();
+}
